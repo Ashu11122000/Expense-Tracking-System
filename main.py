85722fd33ef3d19174ddc9e4d7abc9bd9cf0713d
Ashish Sharma <ashu11vats@gmail.com>
2026-03-20 17:52:41 +0530

    Update main.py file

--- main.py
+++ main.py
@@ -1,38 +1,47 @@
 from expense import add_expense, view_expenses, show_summary
+from utils import validate_amount, validate_type, validate_category
+
 
 def main():
     while True:
-        print("\n------Expense Tracker------")
+        print("\n====== Expense Tracker ======")
         print("1. Add Income/Expense")
-        print("2. View All Records")
+        print("2. View Records")
         print("3. Show Summary")
         print("4. Exit")
-        
-        choice = input("Enter your choice:")
-        
+
+        choice = input("Enter choice: ")
+
         if choice == "1":
-            amount = input("Enter Amount:")
-            category = input("Enter Category:")
-            description = input("Enter Description:")
-            expense_type = input("Type (income/expense): ").lower()
-            
-            if expense_type not in ["income", "expense"]:
-                print("Invalid Type!")
+            amount = validate_amount(input("Enter amount: "))
+            if amount is None:
                 continue
-            
+
+            category = validate_category(input("Enter category: "))
+            if category is None:
+                continue
+
+            description = input("Enter description: ")
+
+            expense_type = validate_type(input("Type (income/expense): "))
+            if expense_type is None:
+                continue
+
             add_expense(amount, category, description, expense_type)
-            
+
         elif choice == "2":
             view_expenses()
-            
+
         elif choice == "3":
             show_summary()
-        
-        elif choice == "4": 
-            print("Exiting....")
-            
+
+        elif choice == "4":
+            print("👋 Exiting...")
+            break
+
         else:
-            print("Invalid choice!")
+            print("❌ Invalid choice")
+
 
 if __name__ == "__main__":
     main()
